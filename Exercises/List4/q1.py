3df2e89c5c32ca20315ae0d2cee48ceb84045210
joao uchoa <joaovictoruchoa01@gmail.com>
2023-10-06 17:58:36 -0300

    lista 4

---
+++ Exercises/List4/q1.py
@@ -0,0 +1,15 @@
+def selecao_ordenacao(vetor):
+    n = len(vetor)
+    for i in range(n):
+        indice_minimo = i
+        for j in range(i + 1, n):
+            if vetor[j] < vetor[indice_minimo]:
+                indice_minimo = j
+        #vetor[i], vetor[indice_minimo] = vetor[indice_minimo], vetor[i]
+        aux = vetor[i]
+        vetor[i] = vetor[indice_minimo]
+        vetor[indice_minimo] = aux
+
+vetor=[5,7,4,3]
+selecao_ordenacao(vetor)
+print(vetor)
